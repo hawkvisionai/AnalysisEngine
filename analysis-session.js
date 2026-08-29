26cb7dcd8adc9ffcaf1866c2f605bf3be04353d6
ian831009 <michael19941009@gmail.com>
2026-08-30 07:31:04 +0800

    Add files via upload

--- analysis-session.js
+++ analysis-session.js
@@ -1,6 +1,6 @@
 (() => {
 "use strict";
-const VERSION="3.4.4";
+const VERSION="3.4.5";
 const POLL_MS=1500;
 const ADMIN_API="https://hawkvision-admin-api.michael19941009.workers.dev";
 const client=window.hvAnalysisAuthClient;
